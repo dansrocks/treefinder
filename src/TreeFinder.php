ed1264da5c7b2a9e0f36224db1ba41c4b4399689
Daniel Cabrera <daniel.cabrera@summais.com>
2019-03-27 00:23:37 +0100

    Primera versión básica de TreeFinder

---
+++ src/TreeFinder.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace TreeFinder;
+
+/**
+ * Class TreeFinder
+ *
+ * @package dansrocks\TreeFinder
+ */
+class TreeFinder
+{
+    /** @var \Closure */
+    private $closure = null;
+
+    /** @var int */
+    private $threshold = null;
+
+    /**
+     * @var array
+     */
+    private $coincidences = [];
+
+    /**
+     * TreeFinder constructor.
+     *
+     * @param \Closure $closure
+     * @param int $threshold
+     */
+    public function __construct(\Closure $closure, int $threshold)
+    {
+        $this->closure = $closure;
+        $this->threshold = $threshold;
+    }
+
+    /**
+     * @param array $left
+     * @param array $right
+     *
+     * @return array
+     */
+    public function findCoincidences(array $left, array $right)
+    {
+        $threshold = $this->getThreshold();
+        foreach ($left as $leftKey => $leftItem) {
+            foreach ($right as $rightKey => $rightItem) {
+                $coincidence = $this->coincidence($leftItem, $rightItem);
+                if ($coincidence >= $threshold) {
+                    $this->appendCoincidence($leftKey, $rightKey, $coincidence, $leftItem, $rightItem);
+                }
+            }
+        }
+        return $this->coincidences;
+    }
+
+    /**
+     * @param $left
+     * @param $right
+     *
+     * @return mixed
+     */
+    private function coincidence($left, $right)
+    {
+        echo "Ejecutando coincidencias entre ({$left}, {$right})\n";
+        return call_user_func($this->closure, $left, $right);
+    }
+
+    /**
+     * @param $leftKey
+     * @param $rightKey
+     * @param $coincidence
+     */
+    private function appendCoincidence($leftKey, $rightKey, $coincidence, $leftItem, $rightItem)
+    {
+        $this->coincidences[] = [
+            'left-pos' => $leftKey,
+            'right-pos' => $rightKey,
+            'left' => $leftItem,
+            'right' => $rightItem,
+            'coincidence' => $coincidence,
+        ];
+    }
+
+    /**
+     * @return\Closure|int
+     */
+    private function getThreshold()
+    {
+        return $this->threshold;
+    }
+}
